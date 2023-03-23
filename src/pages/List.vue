6701f9fabb9f7f5e10aaebe9ea117f6467af2999
transport123 <407395850@qq.com>
2023-03-23 18:18:36 +0800

    what a mess

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -1,13 +1,139 @@
 <script>
 import axios from 'axios'
+import {nextTick, onMounted,ref} from 'vue'
+
 axios.defaults.baseURL='http://192.168.0.26:8080';
 axios.defaults.timeout = 5000;
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+const scrollDiv = ref(null)
+const addDialogRef = ref(null)
+
 export default {
+ 
   data() {
     return {
       showskeleton:true,
-      list:[],
+      list:[{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },{
+        name:"jack",
+        age:15,
+        userId:1,
+        gender:1
+      },],
       editDialogVisible:false,
       addDialogVisible:false,
       deleteDialogVisible:false,
@@ -31,17 +157,24 @@ export default {
   },
   mounted() {
     this.init()
+    console.log(scrollDiv.value)
   },
+
   methods: {
-    init() {
+    init() {//axios.get("/user/showUser")
       this.showskeleton=true;
-      axios.get("/user/showUser").then(response => {
-        console.log(response.data)
-        this.list=response.data;
+      Promise.resolve(this.list).then(response => {
+        console.log(response)
+        this.list=response;
         this.showskeleton=false;
       }).catch((err)=>{
         this.showskeleton=true;
+        console.log(err)
       })
+     
+      console.log("haha")
+
+      // scrollDiv.scrollTop=scrollDiv.scrollTop+e.deltaY;
     },
     forceupdate(){
       this.$forceUpdate();
@@ -55,6 +188,9 @@ export default {
     },
     addUserSubmit()
     {
+      let addDialogRef = this.$refs.addDialogRef;
+      console.log(addDialogRef)
+      addDialogRef.addEventListener('click',this.scrollListener);
       if(!this.checkValidate(this.addform))
       {
         return;
@@ -155,14 +291,28 @@ export default {
         return false;
       }
       return true;
+    },
+    scrollListener(e){
+      // let scrollDiv = this.$refs.scrollDiv;
+      // scrollDiv.scrollTop=scrollDiv.scrollTop+e.deltaY;
+      console.log(e)
+   
+
+    },
+    opened()
+    {
+      console.log(addDialogRef.value);
     }
   },
 }
 </script>
 
 <template>
-  <div>
+  <div style="overflow-y: scroll;height: 120vh;border: 5px solid black;padding-bottom: 10px; padding-right: 100px;"   ref="scrollDiv" >
       <el-skeleton v-show="showskeleton"/>
+      <div style="border: 1px solid green;height: 300px;overflow-y: scroll; padding: 10px;">
+      <div style="border: 1px solid red;height: 600px;"></div>
+      </div>
       <div v-show="!showskeleton">
         <el-table :data="list" style="width: 100%">
         <el-table-column prop="name" label="姓名" width="180" />
@@ -179,9 +329,31 @@ export default {
           </template>
         </el-table-column>
       </el-table>
-      <el-button  type="primary"  size="small" class="elbtn" @click="adduser" style="margin-top: 10px;">添加</el-button>
-      </div>
+      <el-button  type="primary"  size="small" class="elbtn" @click="adduser" style="position: fixed;z-index: 10000;top: 0px;right:0px">添加</el-button>
 
+      </div>
+      <el-dialog title="添加用户" v-model="addDialogVisible" :lock-scroll="false" ref="addDialogRef" @opened="opened">
+        <el-form :model="addform" label-width="80px"  >
+          <el-form-item label="姓名" prop="date">
+            <el-input  auto-complete="off" v-model="addform.name"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄" prop="name">
+            <el-input  auto-complete="off" v-model="addform.age"></el-input>
+          </el-form-item>
+
+          <el-form-item label="性别" prop="gender">
+            <el-radio-group v-model="addform.gender">
+              <el-radio :label="1">男</el-radio>
+              <el-radio :label="0">女</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-form>
+
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="addDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addUserSubmit" v-bind:disabled="addConfirmAble">确 定</el-button>
+        </div>
+      </el-dialog>
 
 
       <el-dialog title="编辑用户" v-model="editDialogVisible" :lock-scroll="false">
@@ -205,28 +377,7 @@ export default {
         </div>
       </el-dialog>
 
-      <el-dialog title="添加用户" v-model="addDialogVisible">
-        <el-form :model="addform" label-width="80px" >
-          <el-form-item label="姓名" prop="date">
-            <el-input  auto-complete="off" v-model="addform.name"></el-input>
-          </el-form-item>
-          <el-form-item label="年龄" prop="name">
-            <el-input  auto-complete="off" v-model="addform.age"></el-input>
-          </el-form-item>
-
-          <el-form-item label="性别" prop="gender">
-            <el-radio-group v-model="addform.gender">
-              <el-radio :label="1">男</el-radio>
-              <el-radio :label="0">女</el-radio>
-            </el-radio-group>
-          </el-form-item>
-        </el-form>
-
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="addDialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="addUserSubmit" v-bind:disabled="addConfirmAble">确 定</el-button>
-        </div>
-      </el-dialog>
+      
 
       <el-dialog title="删除用户" v-model="deleteDialogVisible">
         <div slot="footer" class="dialog-footer">
