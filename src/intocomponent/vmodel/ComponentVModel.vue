07dd17671a5aa739af6c1eaa72457823e922f175
transport123 <407395850@qq.com>
2023-04-10 09:52:03 +0800

    finish component vmodel

--- src/intocomponent/vmodel/ComponentVModel.vue
+++ src/intocomponent/vmodel/ComponentVModel.vue
@@ -1,8 +1,38 @@
 <script setup>
 import MyInput from './CustomInput.vue'
+import {ref} from 'vue'
+const originModel=ref('origin')
+const valueModel=ref('value')
+const customDefaultModel=ref('customDefault')
+const emailModel=ref("4088@qq.com")
+const compute = ref('compute')
+const capitalize=ref('capital')
+
+function valueChanged(event)
+{
+    valueModel.value=event.target.value
+}
+
 </script>
 <template>
-    <MyInput>
-        
-    </MyInput>
+    <div>
+        原生vmodel:{{ originModel }}<br/>
+        <label>原生input:</label>
+        <input type="text" v-model="originModel"/><br/>
+        vmodel实现原理:{{ valueModel }}<br/>
+        <label>vmodel的本质:</label>
+        <!--首先单向绑定，并监听input事件，实时改变model值-->
+        <input type="text" :value="valueModel" @input="valueChanged($event)"/><br/>
+        <!--组件使用vmodel时的原理和此一致，需要单向绑定+事件监听-->
+        自定义组件vmodel:{{ customDefaultModel }}<br/>
+        compute:{{compute }}<br/>
+        邮箱:{{ emailModel }}<br/>
+        <!--vmodel的默认属性为modelValue,其他自定义属性需要使用完整的写法-->
+        <MyInput v-model="customDefaultModel" v-model:email="emailModel" v-model:compute="compute" v-model:capital.capitalize="capitalize">
+        </MyInput><br/>
+
+    </div>
+
+
+    
 </template>
--- src/intocomponent/vmodel/CustomInput.vue
+++ src/intocomponent/vmodel/CustomInput.vue
@@ -1,10 +1,39 @@
 <script setup>
-const props = defineProps([])
-const emits= defineEmits([])
+import {computed} from 'vue'
+const props = defineProps(['modelValue','email','compute','capital','capitalModifiers'])
+//如果想使用v-model的双向绑定，该事件申明时需要带上update前缀
+const emits= defineEmits(['update:modelValue','update:email','update:compute','update:capital'])
+
+const computedValue =computed(
+    {
+        get(){
+            return props.compute
+        },
+        set(value){
+            emits('update:compute',value)
+        }
+    }
+)
+
+function capitalize(capitalValue)
+{
+
+    let value = capitalValue
+    if(props.capitalModifiers.capitalize){
+        value = value.charAt(0).toUpperCase() + value.slice(1)
+    }
+    emits('update:capital',value)
+}
 </script>
 <template>
     <div>
         <h3>这是一个自定义的输入表单</h3>
-        <input type="text">
+        <input type="text" :value="modelValue" @input="$emit('update:modelValue',$event.target.value)">
+        <h3>这是计算属性实现的vmodel</h3>
+        <input type="text" v-model="computedValue">
+        <h3>这是一个邮箱</h3>
+        <input type="text" :value="email" @input="$emit('update:email',$event.target.value)">
+        <h3>这是首字母大写的修饰符</h3>
+        <input type="text" :value="capital" @input="capitalize($event.target.value)">
     </div>
 </template>
